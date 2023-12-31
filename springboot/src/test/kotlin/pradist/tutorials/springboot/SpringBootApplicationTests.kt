3bb7cfe022abdc4926f424b6cd937f968a81308c
Pradist Keawborisut <pradist.ke@gmail.com>
2023-12-31 11:11:55 +0700

    [springboot] feat: add static html for first page

---
+++ springboot/src/test/kotlin/pradist/tutorials/springboot/SpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package pradist.tutorials.springboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
